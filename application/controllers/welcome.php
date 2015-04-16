7624241ec590309d80dbf370f5d7da70a010787c
bagaimana-cara-membuat <info@kursus-php.com>
2015-04-16 14:34:47 +0700

    Part-6

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,25 +2,19 @@
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('model_products');
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data['products'] = $this->model_products->all();
+		$this->load->view('welcome_message', $data);
 	}
+	
+	
 }
 
 /* End of file welcome.php */
